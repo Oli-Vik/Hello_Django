528f1e336b3cd5efe62e9bbe6aff7f08d178748d
User <OlgaMironova494@gmail.com>
2022-11-01 12:01:04 +0300

    Start_files1

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -5,5 +5,10 @@ def about(request):
     a = 5+6
     return render(request,'about.html',{'greeting':a})
 
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request,'reverse.html',{'word':reverse})
+
 def home(request):
-    return render(request,'home.html')
+    return HttpResponse('This is my home page')
